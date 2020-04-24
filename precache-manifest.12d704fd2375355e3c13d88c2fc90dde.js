9aaeccbf4165ed9cf2e9ce411d06fafde3676302
Cassio C <LccassioL@gmail.com>
2020-04-24 16:31:42 -0300

    Test webpage

---
+++ precache-manifest.12d704fd2375355e3c13d88c2fc90dde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cff8ee0a97345953e3d6b0f38f9195f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4efe8165f425588b9f92",
+    "url": "/static/js/2.7ca2c3ed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7ca2c3ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4878e2048ba6b76c3c22",
+    "url": "/static/js/main.8fbc1b4e.chunk.js"
+  },
+  {
+    "revision": "abe870114e8c07c77bd0",
+    "url": "/static/js/runtime-main.ae07b699.js"
+  },
+  {
+    "revision": "993f0a43c8dcf8b5249cda67ae5c5af7",
+    "url": "/static/media/eu.993f0a43.jpg"
+  }
+]);
