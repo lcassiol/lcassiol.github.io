823c98f9c50a13eebce0e1152f8010aa80dfeb55
Cassio C <LccassioL@gmail.com>
2020-06-03 16:21:39 -0300

    Refactoring code extract project to another file

---
+++ source-webpage/src/pages/Projects/projects.js
@@ -0,0 +1,52 @@
+import backendImage from '../../assets/backend.png';
+
+const projects = {
+  nodejs: [
+    {
+      name: 'FastFeet',
+      description: 'Projeto para conclusão do bootcamp 10 da Rocketseat',
+      github: 'https://github.com/lcassiol/nodejs-fastfeet',
+      image: backendImage,
+    },
+    {
+      name: 'Be the hero',
+      description: 'Projeto desenvolvido na semana omni stack da Rocketseat',
+      github: 'https://github.com/lcassiol/nodejs-be-the-hero',
+      image: backendImage,
+    },
+  ],
+  react: [
+    {
+      name: 'FastFeet',
+      description: 'Projeto para conclusão do bootcamp 10 da Rocketseat',
+      github: 'https://github.com/lcassiol/reactjs-fastfeet',
+      image:
+        'https://github.com/lcassiol/reactjs-fastfeet/blob/master/src/assets/screenshots/Deliveries.png?raw=true',
+    },
+    {
+      name: 'Be the hero',
+      description: 'Projeto desenvolvido na semana omni stack da Rocketseat',
+      github: 'https://github.com/lcassiol/reactjs-be-the-hero',
+      image:
+        'https://raw.githubusercontent.com/lcassiol/reactjs-be-the-hero/master/src/assets/screeenshots/print1.png',
+    },
+  ],
+  reactnative: [
+    {
+      name: 'FastFeet',
+      description: 'Projeto para conclusão do bootcamp 10 da Rocketseat',
+      github: 'https://github.com/lcassiol/RN-fastfeet',
+      image:
+        'https://github.com/lcassiol/RN-fastfeet/blob/master/src/assets/screenshots/Dashboardscreen.png?raw=true',
+    },
+    {
+      name: 'Be the hero',
+      description: 'Projeto desenvolvido na semana omni stack da Rocketseat',
+      github: 'https://github.com/lcassiol/RN-be-the-hero',
+      image:
+        'https://raw.githubusercontent.com/lcassiol/RN-be-the-hero/master/assets/screenshots/incidentDetail.jpg',
+    },
+  ],
+};
+
+export default projects;
